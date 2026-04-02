c5039d904bb92062fa88e4aad0243250a33a7b21
Adam-Disanti <disanti.adam@gmail.com>
2026-04-02 17:28:25 -0400

    Fix prod API routing via nginx reverse proxy

--- frontend/src/components/Path.ts
+++ frontend/src/components/Path.ts
@@ -1,8 +1,6 @@
-const app_name = '143.244.149.236';
-
 export function buildPath(route: string): string {
   if (import.meta.env.MODE !== 'development') {
-    return 'http://' + app_name + ':5001/' + route;
+    return '/' + route;
   }
   else {
     return 'http://localhost:5001/' + route;
